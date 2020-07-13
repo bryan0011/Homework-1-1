fbf57a78e8e14c81193094b52bfb9a448f00050e
BryanKuo <bryan0011@gmail.com>
2020-07-13 18:09:26 +0800

    First Vision

---
+++ Homework 1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 1-1
+//
+//  Created by Bryan Kuo on 2020/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
